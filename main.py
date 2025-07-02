8fbfa6378ec9f700cfb647f1e9da526d15f1f5d7
sumanasri <pailasumanasri@gmail.com>
2025-07-02 16:43:41 -0400

    Set up FastAPI with /ping route

---
+++ main.py
@@ -0,0 +1,18 @@
+# /backend/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow frontend to connect
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict to frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
